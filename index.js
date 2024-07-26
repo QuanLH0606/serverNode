737b9931b3b669c18018ea2bdb4c2577ee4dc860
quan.luonghoang <quan.luonghoang@VNIDSG00LTITD04>
2024-07-26 09:59:12 +0700

    update code server

--- index.js
+++ index.js
@@ -1,99 +1,43 @@
 const express = require('express');
-const bodyParser = require('body-parser');
-const swaggerJsdoc = require('swagger-jsdoc');
-const swaggerUi = require('swagger-ui-express');
-const request = require('request');
+const cors = require('cors'); // Thay 'request' bằng 'require'
+const request = require('request'); // Giữ 'require' cho 'request'
+const axios = require('axios');
 const app = express();
 const port = 3000;
 
-// Swagger setup
-const options = {
-  definition: {
-    openapi: '3.0.0',
-    info: {
-      title: 'Express API with Swagger',
-      version: '1.0.0',
-      description: 'A simple API application made with Express and documented with Swagger',
-    },
-    servers: [
-      {
-        url: `http://localhost:${port}`,
-      },
-    ],
-  },
-  apis: ['./index.js'], // Specify your API docs file
-};
-
-const specs = swaggerJsdoc(options);
-app.use(cors({
-  origin: '*', // Hoặc chỉ định các miền cụ thể thay vì '*' để tăng cường bảo mật
-}));
-// Middleware
-app.use(bodyParser.json()); // To parse JSON bodies
-app.use('/swagger', swaggerUi.serve, swaggerUi.setup(specs));
-
-/**
- * @swagger
- * /:
- *   get:
- *     summary: Welcome message
- *     responses:
- *       200:
- *         description: Returns a welcome message
- */
+app.use(cors());
+app.use(express.json());
 app.get('/', (req, res) => {
     res.send('Server Node');
 });
 
-/**
- * @swagger
- * /api/unlock-user:
- *   post:
- *     summary: Unlock user by email and username
- *     requestBody:
- *       description: User information to unlock
- *       required: true
- *       content:
- *         application/json:
- *           schema:
- *             type: object
- *             properties:
- *               EMAIL:
- *                 type: string
- *               USER:
- *                 type: string
- *     responses:
- *       200:
- *         description: Unlock user successfully
- *       500:
- *         description: Internal server error
- */
 app.post('/api/unlock-user', (req, res) => {
     const { EMAIL, USER } = req.body;
-
-    var options = {
-        method: 'POST',
-        url: 'http://10.1.6.13:8002/sap/bc/rest/zrest/USER?sap-client=103',
-        headers: {
-            'Content-Type': 'application/json',
-            'Authorization': 'Basic QVBJQ09OTkVDVDppVVd7aCR3c0xzeiVGZXMyRVh3cXs9cEJwS1pFcWJjZGtdLyt+VVRZ',
-            'Cookie': 'SAP_SESSIONID_DEV_103=jgbb2HZ4XZmyw0I0ZfKwBlVF4jNKahHvrEgAUFawrYI%3d; sap-usercontext=sap-client=103'
-        },
-        body: JSON.stringify({
-            "EMAIL": EMAIL,
-            "USER": USER
-        })
+    let data = JSON.stringify({
+      "EMAIL": EMAIL,
+      "USER": USER
+    });
+    let config = {
+      method: 'post',
+      maxBodyLength: Infinity,
+      url: 'http://10.1.6.13:8002/sap/bc/rest/zrest/USER?sap-client=103',
+      headers: { 
+        'Content-Type': 'application/json', 
+        'Authorization': 'Basic QVBJQ09OTkVDVDppVVd7aCR3c0xzeiVGZXMyRVh3aXs9cEJwS1pFcWJjZGtdLyt+VVRZ', 
+        'Cookie': 'SAP_SESSIONID_DEV_103=gNUb5JXdUwePHooWGJWg5XsAUNtK-hHviTMAUFawrYI%3d; sap-usercontext=sap-client=103'
+      },
+      data : data
     };
 
-    request(options, function (error, response) {
-        if (error) {
-            console.error(error);
-            return res.status(500).send('Something went wrong!');
-        }
-        res.status(response.statusCode).send(response.body);
-    });
+    axios.request(config)
+      .then((response) => {
+        console.log(JSON.stringify(response.data));
+      })
+      .catch((error) => {
+        console.log(error);
+      });
 });
 
 app.listen(port, () => {
-    console.log(`Server is running at http://localhost:${port}/swagger`);
+    console.log(`Server is running at http://localhost:${port}`);
 });
